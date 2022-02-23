db789585654a60b0d5f8a86779af07a5def64058
= <505825@vistacollege.nl>
2022-02-23 11:23:20 +0100

    fotos + css

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vista Cars</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image" href="./fotos/auto.png">
 </head>
 <body>
     
