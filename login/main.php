59e3fd9e280e94a40aced8f0a40bb017744e5a01
= <505825@vistacollege.nl>
2022-03-14 10:48:27 +0100

    nh

--- login/main.php
+++ login/main.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include("../includes/functions.php");
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -6,14 +11,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vista Cars</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" type="image" href="./fotos/icon2.png">
+    <link rel="stylesheet" href="../styles.css">
+    <link rel="icon" type="image" href="../fotos/icon2.png">
 </head>
 
 <body class="background_main">
 
     <div class="header_image">
-        <img class="logo" src="./fotos/logo2.png" alt="Vista Cars logo">
+        <img class="logo" src="../fotos/logo2.png" alt="Vista Cars logo">
     </div>
     <div class="header_text">
         <h1 class="title"> Vista Cars</h1>
@@ -23,7 +28,7 @@
     <div class="content_main">
         <div class="buttons">
 
-            <form action="afsprakensysteem/index.php">
+            <form action="../afsprakensysteem/index.php">
                 <input class="button_main" type="submit" value="Afspraken">
             </form>
 
@@ -31,7 +36,7 @@
                 <input class="button_main" type="submit" value="Admin toevoegen">
             </form>
 
-            <form action="index.php">
+            <form action="../includes/logout.php">
                 <input class="button_main" type="submit" value="Uitloggen">
             </form>
 
